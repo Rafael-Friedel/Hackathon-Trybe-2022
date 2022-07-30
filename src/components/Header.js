75dc2e4e711f18220d3f7488a9ebd47cb8a06c30
Anjel Diniz <anjeldiniz@gmail.com>
2022-07-30 18:54:01 -0300

    style: css no header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,24 +4,35 @@ import logo from '../images/logo.png';
 import search from '../images/Busca.svg';
 import profile from '../images/logado.svg';
 import bag from '../images/Group.svg';
+import style from '../styles/header.module.css';
 
 const Header = () => (
-  <header>
-    <img src={ logo } alt="Logotipo da Wine clube" />
-    <nav>
-      <ul>
-        <Link to="/">Clube</Link>
-        <Link to="/products">Loja</Link>
-        <Link to="/">Produtores</Link>
-        <Link to="/products">Ofertas</Link>
-        <Link to="/">Eventos</Link>
-        <Link to="/">Baixe o app</Link>
-      </ul>
+  <header className={ style.header }>
+    <img className={ style.logo } src={ logo } alt="Logotipo da Wine clube" />
+    <nav className={ style.nav }>
+      <Link className={ style.link } to="/">
+        Clube
+      </Link>
+      <Link className={ style.link } to="/products">
+        Loja
+      </Link>
+      <Link className={ style.link } to="/">
+        Produtores
+      </Link>
+      <Link className={ style.link } to="/products">
+        Ofertas
+      </Link>
+      <Link className={ style.link } to="/">
+        Eventos
+      </Link>
+      <Link className={ style.link } to="/">
+        Baixe o app
+      </Link>
     </nav>
-    <aside>
-      <img src={ search } alt="Pesquisar" />
-      <img src={ profile } alt="Fazer login" />
-      <img src={ bag } alt="Ver carrinho" />
+    <aside className={ style.aside }>
+      <img className={ style.img } src={ search } alt="Pesquisar" />
+      <img className={ style.img } src={ profile } alt="Fazer login" />
+      <img className={ style.img } src={ bag } alt="Ver carrinho" />
     </aside>
   </header>
 );
