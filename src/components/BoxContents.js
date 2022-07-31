de6d2f9696e109126555bd90cf15295522f8121f
debora <debora.r.serra@gmail.com>
2022-07-30 22:57:35 -0300

    style: adiciona css ao boxContent

--- src/components/BoxContents.js
+++ src/components/BoxContents.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import wines from '../images/vinhos.svg';
+import magazine from '../images/Revista.svg';
+import cortaGotas from '../images/corta_gotas.png';
+import style from '../styles/boxContent.module.css';
 
 const BoxContent = () => (
-  <section>
+  <section className={ style.main_sect }>
     <h2>O que vai na sua WineBox?</h2>
-    <section>
+    <section className={ style.content_sect }>
       <section>
-        <img src="" alt="Duas garrafas de vinho" />
+        <img src={ wines } alt="Duas garrafas de vinho" />
         <h3>2 vinhos</h3>
         <p>Você receberá duas garrafas inéditas selecionadas pelos nossos winehunters</p>
       </section>
-      <img src="" alt="Mais" />
       <section>
-        <img src="" alt="Uma revista" />
+        <img src={ magazine } alt="Uma revista" />
         <h3>1 revista</h3>
         <p>
           Conteúdo exclusivo para sócios, como armonizações,
@@ -19,9 +22,8 @@ const BoxContent = () => (
           dicas e curiosidades sobre o mundo do vinho.
         </p>
       </section>
-      <img src="" alt="Mais" />
       <section>
-        <img src="" alt="Um folder corta gotas" />
+        <img src={ cortaGotas } alt="Um folder corta gotas" />
         <h3>1 corta-gotas</h3>
         <p>
           Todo mês uma arte diferente para você colecionar
