bbf81cc52edd203cfe98261d895bed8d15986288
debora <debora.r.serra@gmail.com>
2022-07-31 01:34:48 -0300

    style: adiciona css à sessão de vinhos

--- src/components/Wines.js
+++ src/components/Wines.js
@@ -1,13 +1,15 @@
 import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import { Context } from '../provider/provider';
 import WineCard from './WineCard';
+import style from '../styles/wines.module.css';
 
 const Wines = () => {
   const { wines } = useContext(Context);
   return (
-    <section>
-      <h3>Vinhos recomendados</h3>
-      <section>
+    <section className={ style.main_sect }>
+      <Link to="/products" className={ style.title }>Vinhos recomendados</Link>
+      <section className={ style.cards_sect }>
         {wines.map((wine) => (
           <WineCard wine={ wine } key={ wine.id } />
         ))}
